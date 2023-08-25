f0630ea536b84debb6bccc3e44f5bdeae5dcf100
syjeong <syjeong@petprice.kr>
2023-08-26 02:33:22 +0900

    라우팅 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from './pages/main/main';
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Main name={'Main'} />} />
+			<Route path="/skyamg" element={<Main name={'Main'} />} />
 		</Routes>
 	);
 }
