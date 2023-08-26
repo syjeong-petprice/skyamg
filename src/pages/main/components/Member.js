5611a162588ff9e686b4b7e49338c24327016f1f
syjeong <syjeong@petprice.kr>
2023-08-27 00:46:06 +0900

    수의사 프로필 이미지 적용

--- src/pages/main/components/Member.js
+++ src/pages/main/components/Member.js
@@ -4,14 +4,123 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import bgSky from '../../../images/resource/images/bg_Sky.png';
+import vet1 from '../../../images/vet/doc_cheonwoojin.jpg';
+import vet2 from '../../../images/vet/doc_oise.png';
+import vet3 from '../../../images/vet/doc_moonjongsun.png';
+import vet4 from '../../../images/vet/doc_jeongdaseul.png';
+import vet5 from '../../../images/vet/doc_kimdohee.png';
+import vet6 from '../../../images/vet/doc_choidongjun.png';
+import vet7 from '../../../images/vet/doc_parksulki.png';
+import vet8 from '../../../images/vet/doc_parkjoohyang.png';
+import vet9 from '../../../images/vet/doc_choiyujin.png';
+import vet10 from '../../../images/vet/doc_jeongtaegyu.png';
+import vet11 from '../../../images/vet/doc_yoongihong.png';
+import vet12 from '../../../images/vet/doc_jangminhyuk.png';
+import vet13 from '../../../images/vet/doc_leesunback.png';
+import vet14 from '../../../images/vet/doc_seoseungwon.png';
+import vet15 from '../../../images/vet/doc_songjaeseong.png';
+
+const vetInfo = [
+	{
+		id: 1,
+		position: '대표원장',
+		name: '천우진',
+		img: vet1,
+	},
+	{
+		id: 2,
+		position: '대표원장',
+		name: '오이세',
+		img: vet2,
+	},
+	{
+		id: 3,
+		position: '원장',
+		name: '문종선',
+		img: vet3,
+	},
+	{
+		id: 4,
+		position: '부원장',
+		name: '정다슬',
+		img: vet4,
+	},
+	{
+		id: 5,
+		position: '외과과장',
+		name: '김도희',
+		img: vet5,
+	},
+	{
+		id: 6,
+		position: '외과과장',
+		name: '최동준',
+		img: vet6,
+	},
+	{
+		id: 7,
+		position: '내과과장',
+		name: '박설기',
+		img: vet7,
+	},
+	{
+		id: 8,
+		position: '영상의학과장',
+		name: '박주향',
+		img: vet8,
+	},
+	{
+		id: 9,
+		position: '진료과장',
+		name: '최유진',
+		img: vet9,
+	},
+	{
+		id: 10,
+		position: '한방응급의학과장',
+		name: '정태규',
+		img: vet10,
+	},
+	{
+		id: 11,
+		position: '진료수의사',
+		name: '윤기홍',
+		img: vet11,
+	},
+	{
+		id: 12,
+		position: '진료수의사',
+		name: '장민혁',
+		img: vet12,
+	},
+	{
+		id: 13,
+		position: '진료수의사',
+		name: '이선백',
+		img: vet13,
+	},
+	{
+		id: 14,
+		position: '진료수의사',
+		name: '서승원',
+		img: vet14,
+	},
+	{
+		id: 15,
+		position: '진료수의사',
+		name: '송재성',
+		img: vet15,
+	},
+];
 
 function Member() {
 	const [animate, setAnimate] = useState(false);
+	const [show, setShow] = useState(5);
 	const settings = {
-		dots: true,
+		dots: false,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 4,
+		slidesToShow: show,
 		slidesToScroll: 1,
 		autoplay: true,
 		autoplaySpeed: 2000, // 오토플레이 간격을 조절할 수 있습니다.
@@ -39,6 +148,29 @@ function Member() {
 			window.removeEventListener('scroll', handleScroll);
 		};
 	}, []);
+
+	useEffect(() => {
+		const handleScreenWidth = () => {
+			if (window.innerWidth > 1180) {
+				setShow(5);
+			}
+			if (window.innerWidth < 1180) {
+				setShow(4);
+			}
+			if (window.innerWidth < 1040) {
+				setShow(3);
+			}
+			if (window.innerWidth < 500) {
+				setShow(2);
+			}
+		};
+
+		window.addEventListener('resize', handleScreenWidth);
+
+		return () => {
+			window.removeEventListener('resize', handleScreenWidth);
+		};
+	}, []);
 	return (
 		<MemberContainer>
 			<TitleWrapper>
@@ -54,21 +186,16 @@ function Member() {
 			<SliderWrapper>
 				<div className={animate ? 'animate' : ''}>
 					<StyledSlider {...settings}>
-						<div>
-							<h3>1</h3>
-						</div>
-						<div>
-							<h3>2</h3>
-						</div>
-						<div>
-							<h3>3</h3>
-						</div>
-						<div>
-							<h3>4</h3>
-						</div>
-						<div>
-							<h3>5</h3>
-						</div>
+						{vetInfo.map((vet) => (
+							<div className="vetProfile" key={vet.id}>
+								<div>
+									<img alt={vet.name} src={vet.img} />
+								</div>
+								<p>
+									{vet.position} {vet.name}
+								</p>
+							</div>
+						))}
 					</StyledSlider>
 				</div>
 			</SliderWrapper>
@@ -141,10 +268,31 @@ const TitleWrapper = styled.div`
 `;
 
 const StyledSlider = styled(Slider)`
+	.vetProfile {
+		div {
+			width: 100%;
+			height: 100%;
+
+			img {
+				width: 100%;
+				height: auto;
+			}
+		}
+		p {
+			color: #fff;
+			font-size: 0.8rem;
+			font-weight: bold;
+			text-align: center;
+		}
+	}
 	.slick-list {
-		width: 65vw;
-		height: 40vh;
-		background-color: #f0f9ff;
+		width: 85vw;
+		height: 45vh;
+		/* background-color: #f0f9ff; */
+
+		@media screen and (max-width: 690px) {
+			height: 30vh;
+		}
 	}
 	.slick-prev:before,
 	.slick-next:before {
