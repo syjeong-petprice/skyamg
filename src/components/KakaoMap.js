406797971552858e1d6277daf2ab76c8e561ee4e
syjeong <syjeong@petprice.kr>
2023-08-28 14:43:59 +0900

    좌표수정

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,27 +1,27 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 const { kakao } = window;
 
 function KakaoMap() {
-  useEffect(() => {
-    const container = document.getElementById("map");
-    const options = {
-      center: new kakao.maps.LatLng(37.454233, 126.707068),
-      level: 4,
-    };
-    const markerPosition = new kakao.maps.LatLng(37.454233, 126.707068);
-    const marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
+	useEffect(() => {
+		const container = document.getElementById('map');
+		const options = {
+			center: new kakao.maps.LatLng(37.454012, 126.707794),
+			level: 3,
+		};
+		const markerPosition = new kakao.maps.LatLng(37.454012, 126.707794);
+		const marker = new kakao.maps.Marker({
+			position: markerPosition,
+		});
 
-    const map = new kakao.maps.Map(container, options);
-    marker.setMap(map);
-  }, []);
+		const map = new kakao.maps.Map(container, options);
+		marker.setMap(map);
+	}, []);
 
-  return (
-    <>
-      <div id="map" style={{ width: "50%", height: "50%" }}></div>
-    </>
-  );
+	return (
+		<>
+			<div id="map" style={{ width: '50%', height: '50%' }}></div>
+		</>
+	);
 }
 
 export default KakaoMap;
