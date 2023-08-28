67e192fa3c3c907a87e95630263a31a1983b998a
syjeong <syjeong@petprice.kr>
2023-08-28 15:56:30 +0900

    위치 연락처 추가

--- src/pages/main/components/Location.js
+++ src/pages/main/components/Location.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { styled, keyframes } from 'styled-components';
 import KakaoMap from '../../../components/KakaoMap';
-import skyBuilding from '../../../images/resource/images/스카이야경.jpeg';
+import skyBuilding from '../../../images/resource/images/스카이야경2.jpg';
 
 function Location() {
 	const [animate, setAnimate] = useState(false);
@@ -12,19 +12,30 @@ function Location() {
 					className={animate ? 'locationWrapper animate' : 'locationWrapper'}
 				>
 					<div className="addrWrapper">
-						<h3>오시는 길</h3>
+						<div className="heading">
+							<h3>Contact</h3>
+						</div>
 						<p>
-							인천광역시 남동구 남동대로 799번길 34 구월지웰시티푸르지오 C동 3층
+							인천광역시 남동구 남동대로 799번길 34
+							<br /> 구월지웰시티푸르지오 C동 3층
 						</p>
+						<p className="phone">☎️ 032-715-7959</p>
 					</div>
-					<div className="phoneWrapper">
-						<h3>연락처</h3>
-						<p>032-715-7959</p>
+					<div className="parkWrapper">
+						<div className="heading">
+							<h3>주차안내</h3>
+						</div>
+						<p>어디어디에 주차하시면 됩니다.</p>
 					</div>
 				</div>
-				<div className="imgWrapper"></div>
 			</TextWrapper>
-			<KakaoMap />
+			<MapWrapper>
+				<div className="contactWrapper">
+					<div className="innerWrapper">
+						<KakaoMap />
+					</div>
+				</div>
+			</MapWrapper>
 		</LocationContainer>
 	);
 }
@@ -37,36 +48,83 @@ const LocationContainer = styled.section`
 	* {
 		margin: 0;
 	}
+
+	.heading {
+		padding-left: 30px;
+		margin-top: 35px;
+		h3 {
+			font-size: 1.8rem;
+		}
+	}
+	p {
+		font-size: 1.2rem;
+		padding-left: 30px;
+	}
+
+	@media screen and (max-width: 690px) {
+		flex-direction: column;
+	}
 `;
 
 const TextWrapper = styled.div`
 	width: 50%;
 	height: 100%;
 
-	.locationWrapper {
+	@media screen and (max-width: 690px) {
 		width: 100%;
 		height: 50%;
-
-		.addrWrapper {
-			margin-bottom: 25px;
-		}
-
-		h3 {
-			font-size: 2rem;
-		}
-		p {
-			font-size: 1.5rem;
-		}
 	}
 
-	.imgWrapper {
+	.locationWrapper {
+		width: 100%;
+		height: 100%;
+		background-color: #2963a3;
+		color: #fff;
+		background-image: url(${skyBuilding});
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+	}
+	/* .imgWrapper {
 		margin: 0;
 		width: 100%;
 		height: 50%;
 		background-image: url(${skyBuilding});
 		background-repeat: no-repeat;
 		background-size: cover;
-		/* background-position: 0 -110px; */
+		background-position: center;
+
+		@media screen and (max-width: 550px) {
+			display: none;
+		}
+	} */
+`;
+
+const MapWrapper = styled.div`
+	width: 50%;
+	height: 100%;
+	@media screen and (max-width: 690px) {
+		width: 100%;
+		height: 50%;
+	}
+
+	.contactWrapper {
+		width: 100%;
+		height: 100%;
+		background-color: #fffccb;
+
+		.innerWrapper {
+			width: 100%;
+			height: 100%;
+
+			@media screen and (max-width: 690px) {
+				height: 100%;
+			}
+		}
 	}
 `;
 
