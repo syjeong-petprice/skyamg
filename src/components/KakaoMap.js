2dc3487b738f062f8c68081bbf798bb237e5121c
yseulgi <ysg213@petprice.kr>
2023-08-28 11:38:27 +0900

    위/경도 수정

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 const { kakao } = window;
 
 function KakaoMap() {
-	useEffect(() => {
-		const container = document.getElementById('map');
-		const options = {
-			center: new kakao.maps.LatLng(126.707068, 37.454233),
-			level: 3,
-		};
-		const markerPosition = new kakao.maps.LatLng(126.707068, 37.454233);
-		const marker = new kakao.maps.Marker({
-			position: markerPosition,
-		});
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new kakao.maps.LatLng(37.454233, 126.707068),
+      level: 4,
+    };
+    const markerPosition = new kakao.maps.LatLng(37.454233, 126.707068);
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
 
-		const map = new kakao.maps.Map(container, options);
-		marker.setMap(map);
-	}, []);
+    const map = new kakao.maps.Map(container, options);
+    marker.setMap(map);
+  }, []);
 
-	return (
-		<>
-			<div id="map" style={{ width: '50%', height: '50%' }}></div>
-		</>
-	);
+  return (
+    <>
+      <div id="map" style={{ width: "50%", height: "50%" }}></div>
+    </>
+  );
 }
 
 export default KakaoMap;
