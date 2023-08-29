01a8f3c2b8f7d772c3165c3db9f3c777298d7e32
petprice-beom <120349355+petprice-beom@users.noreply.github.com>
2023-08-29 15:33:58 +0900

    location 끝

--- src/pages/main/components/Location.js
+++ src/pages/main/components/Location.js
@@ -1,62 +1,65 @@
-import { useState, useEffect } from 'react';
-import { styled, keyframes } from 'styled-components';
-import KakaoMap from '../../../components/KakaoMap';
-import skyBuilding from '../../../images/resource/images/스카이야경2.jpg';
+import { useState, useEffect, useRef } from "react";
+import { styled, keyframes } from "styled-components";
+import KakaoMap from "../../../components/KakaoMap";
+import skyBuilding from "../../../images/resource/images/스카이야경2.jpg";
 
 function Location() {
-	const [animate, setAnimate] = useState(false);
-	useEffect(() => {
-		const handleScroll = () => {
-			// 예시: 화면의 중간에 도달했을 때 애니메이션을 실행하려면
-			const midScreen = window.innerHeight * 6.2;
+  const [animate, setAnimate] = useState(false);
+  const [windowWidth, setWindowWidth] = useState();
+  const componentRef = useRef(null);
+  useEffect(() => {
+    const handleScroll = () => {
+      // 예시: 화면의 중간에 도달했을 때 애니메이션을 실행하려면
+      // const midScreen = window.innerHeight * 6.2;
+      const componentTop = componentRef.current.getBoundingClientRect().top;
 
-			if (window.scrollY > midScreen) {
-				setAnimate(true);
-			} else {
-				setAnimate(false);
-			}
-		};
+      if (componentTop < (window.innerHeight * 2) / 3) {
+        setAnimate(true);
+      } else {
+        setAnimate(false);
+      }
+    };
 
-		window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-		return () => {
-			// 컴포넌트 언마운트 시 이벤트 리스너 제거
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, []);
-	return (
-		<LocationContainer>
-			<TextWrapper>
-				<div
-					className={animate ? 'locationWrapper animate' : 'locationWrapper'}
-				>
-					<div className="addrWrapper">
-						<div className="heading">
-							<h3>Contact</h3>
-						</div>
-						<p>
-							인천광역시 남동구 남동대로 799번길 34
-							<br /> 구월지웰시티푸르지오 C동 3층
-						</p>
-						<p className="phone">☎️ 032-715-7959</p>
-					</div>
-					<div className="parkWrapper">
-						<div className="heading">
-							<h3>주차안내</h3>
-						</div>
-						<p>어디어디에 주차하시면 됩니다.</p>
-					</div>
-				</div>
-			</TextWrapper>
-			<MapWrapper>
-				<div className={animate ? 'contactWrapper animate' : 'contactWrapper'}>
-					<div className="innerWrapper">
-						<KakaoMap />
-					</div>
-				</div>
-			</MapWrapper>
-		</LocationContainer>
-	);
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <LocationContainer ref={componentRef}>
+      <TextWrapper>
+        <div
+          className={animate ? "locationWrapper animate" : "locationWrapper"}
+        >
+          <div className="addrWrapper">
+            <div className="heading">
+              <h3>Contact</h3>
+            </div>
+            <p>
+              인천광역시 남동구 남동대로 799번길 34
+              <br /> 구월지웰시티푸르지오 C동 3층
+            </p>
+            <p className="phone">☎️ 032-715-7959</p>
+          </div>
+          <div className="parkWrapper">
+            <div className="heading">
+              <h3>주차안내</h3>
+            </div>
+            <p>어디어디에 주차하시면 됩니다.</p>
+          </div>
+        </div>
+      </TextWrapper>
+      <MapWrapper>
+        <div className={animate ? "contactWrapper animate" : "contactWrapper"}>
+          <div className="innerWrapper">
+            <KakaoMap />
+          </div>
+        </div>
+      </MapWrapper>
+    </LocationContainer>
+  );
 }
 
 const slideRight = keyframes`
@@ -82,71 +85,71 @@ const slideLeft = keyframes`
 `;
 
 const LocationContainer = styled.section`
-	width: 100%;
-	height: 92vh;
-	display: flex;
+  width: 100%;
+  height: 92vh;
+  display: flex;
 
-	* {
-		margin: 0;
-	}
+  * {
+    margin: 0;
+  }
 
-	.heading {
-		padding-left: 30px;
-		margin-top: 35px;
-		h3 {
-			font-size: 1.8rem;
-			@media screen and (max-width: 690px) {
-				font-size: 1.5rem;
-			}
-			@media screen and (max-width: 500px) {
-				font-size: 1.4rem;
-			}
-		}
-	}
-	p {
-		font-size: 1.2rem;
-		padding-left: 30px;
-		@media screen and (max-width: 690px) {
-			font-size: 1rem;
-		}
-		@media screen and (max-width: 500px) {
-			font-size: 0.9rem;
-		}
-	}
+  .heading {
+    padding-left: 30px;
+    margin-top: 35px;
+    h3 {
+      font-size: 1.8rem;
+      @media screen and (max-width: 690px) {
+        font-size: 1.5rem;
+      }
+      @media screen and (max-width: 500px) {
+        font-size: 1.4rem;
+      }
+    }
+  }
+  p {
+    font-size: 1.2rem;
+    padding-left: 30px;
+    @media screen and (max-width: 690px) {
+      font-size: 1rem;
+    }
+    @media screen and (max-width: 500px) {
+      font-size: 0.9rem;
+    }
+  }
 
-	@media screen and (max-width: 690px) {
-		flex-direction: column;
-	}
+  @media screen and (max-width: 690px) {
+    flex-direction: column;
+  }
 `;
 
 const TextWrapper = styled.div`
-	width: 50%;
-	height: 100%;
+  width: 50%;
+  height: 100%;
 
-	.animate {
-		animation: ${slideLeft} 3s ease;
-	}
+  .animate {
+    animation: ${slideLeft} 3s ease;
+  }
 
-	@media screen and (max-width: 690px) {
-		width: 100%;
-		height: 50%;
-	}
+  @media screen and (max-width: 690px) {
+    width: 100%;
+    height: 50%;
+  }
 
-	.locationWrapper {
-		width: 100%;
-		height: 100%;
-		background-color: #2963a3;
-		color: #fff;
-		background-image: url(${skyBuilding});
-		background-repeat: no-repeat;
-		background-size: cover;
-		background-position: center;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: flex-start;
-	}
-	/* .imgWrapper {
+  .locationWrapper {
+    width: 100%;
+    height: 100%;
+    background-color: #2963a3;
+    color: #fff;
+    background-image: url(${skyBuilding});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+  }
+  /* .imgWrapper {
 		margin: 0;
 		width: 100%;
 		height: 50%;
@@ -162,30 +165,30 @@ const TextWrapper = styled.div`
 `;
 
 const MapWrapper = styled.div`
-	width: 50%;
-	height: 100%;
-	.animate {
-		animation: ${slideRight} 3s ease;
-	}
-	@media screen and (max-width: 690px) {
-		width: 100%;
-		height: 50%;
-	}
+  width: 50%;
+  height: 100%;
+  .animate {
+    animation: ${slideRight} 3s ease;
+  }
+  @media screen and (max-width: 690px) {
+    width: 100%;
+    height: 50%;
+  }
 
-	.contactWrapper {
-		width: 100%;
-		height: 100%;
-		background-color: #fffccb;
+  .contactWrapper {
+    width: 100%;
+    height: 100%;
+    background-color: #fffccb;
 
-		.innerWrapper {
-			width: 100%;
-			height: 100%;
+    .innerWrapper {
+      width: 100%;
+      height: 100%;
 
-			@media screen and (max-width: 690px) {
-				height: 100%;
-			}
-		}
-	}
+      @media screen and (max-width: 690px) {
+        height: 100%;
+      }
+    }
+  }
 `;
 
 export default Location;
