6d9942a86f4d5f313307e100a74179f8483f02c7
yseulgi <ysg213@petprice.kr>
2023-08-29 20:54:37 +0900

    라우트 변경 시 스크롤 최상단으로

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Main from "./pages/main/main";
 import Noti from "./pages/notice/notice";
 import Doctor from "./pages/doctor/doctor";
 import Subject from "./pages/subject/subject";
 
 function App() {
+  const { pathname } = useLocation();
+
+  // 라우트 이동시 스크롤 최상단으로 이동
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  // 라우트 변경 후 스크롤 최상단으로 이동
+  useEffect(() => {
+    scrollToTop();
+    console.log("test");
+  }, [pathname]);
+
   return (
     <Routes>
       <Route path="/skyamg" element={<Main name={"Main"} />} />
